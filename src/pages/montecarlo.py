67c44946e6452691d2adb5ece4d807bdfd6fe252
Kayes <kayeschong@gmail.com>
2020-03-24 14:07:44 +0800

    Convert vectorized code to loop to reduce memory footprint

--- src/pages/montecarlo.py
+++ src/pages/montecarlo.py
@@ -7,27 +7,30 @@ import pandas as pd
 
 def run_montecarlo():
     st.subheader('Monte Carlo Simulation')
-    periods = st.slider('Days to simulate', min_value=100, max_value=500, value=100, step=100)
+    periods = st.slider('Days to simulate', min_value=100, max_value=400, value=100, step=100)
     start_price = st.number_input('Start Price', min_value=50, value=50, step=10)
     simulations =  st.number_input('Number of futures', min_value=0, max_value=400, value=200, step=50)
 
 
-    price_changes = np.random.randn(periods, simulations)
-    price = start_price + price_changes.cumsum(axis=0)
-
     if st.button(f'Simulate 1 future over {periods} days'):
+
+        price_changes = np.random.randn(1, 1)
+        price = start_price + price_changes
         
         st.markdown('Daily price changes')
-        price_change_chart = st.line_chart(price_changes[:1, 0])
+        price_change_chart = st.line_chart(price_changes)
 
         st.markdown('Prices')
-        price_chart = st.line_chart(price[:1, 0])
+        price_chart = st.line_chart(price)
         progress_bar = st.progress(0)
         status_text = st.empty()
 
         for i in range(1, periods + 1):
-            price_change_chart.add_rows(price_changes[i-1:i, 0])
-            price_chart.add_rows(price[i-1:i, 0])
+            price_changes = np.random.randn(1, 1)
+            price = price + price_changes
+
+            price_change_chart.add_rows(price_changes)
+            price_chart.add_rows(price)
             
             if i % (periods//100) == 0:
                 status = i // (periods//100)
@@ -41,12 +44,11 @@ def run_montecarlo():
         st.button("Clear chart")
 
 
-
     if st.button(f'Simulate {simulations} futures over {periods} days'):
-        price_changes = np.random.randn(periods, simulations)
-        price = start_price + price_changes.cumsum(axis=0)
+        price_changes = np.random.randn(1, simulations)
+        price = start_price + price_changes
 
-        price_chart = st.line_chart(price[:1, :])
+        price_chart = st.line_chart(price)
 
         end_price = alt.Chart(pd.DataFrame(price[0, :], columns=["End Prices"])).mark_bar().encode(
                               alt.X("End Prices", bin=alt.Bin(step=5)),
@@ -59,13 +61,16 @@ def run_montecarlo():
         status_text = st.empty()
 
         for i in range(1, periods + 1):
-            price_chart.add_rows(price[i-1:i, :])
+            price = price + np.random.randn(1, simulations)
+
+            price_chart.add_rows(price)
             
-            end_price = alt.Chart(pd.DataFrame(price[i-1, :], columns=["End Prices"])).mark_bar().encode(
+            end_price = alt.Chart(pd.DataFrame(price[0, :], columns=["End Prices"])).mark_bar().encode(
                                   alt.X("End Prices", bin=alt.Bin(step=5)),
                                   y='count()',
                                   )
             end_price_chart.altair_chart(end_price)
+
             if i % (periods//100) == 0:
                 status = i // (periods//100)
                 status_text.text(f"{status}% Complete")
@@ -76,8 +81,6 @@ def run_montecarlo():
 
         if simulations == 0:
             st.write("I guess you've got no future.")
-
-
         
         st.button("Clear chart")
         
